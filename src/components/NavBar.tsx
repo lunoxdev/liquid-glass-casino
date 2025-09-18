c2ed73bbca101212c18efe2a8fd5e0018babadc8
lunoxdev <lunox.code@gmail.com>
2025-09-18 16:27:47 -0600

    Refactor NavBar component layout for improved responsiveness and styling; integrate AuthButton directly into the main container.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,13 +6,11 @@ import Link from 'next/link';
 export default function NavBar() {
     return (
         <nav className="p-3 bg-black">
-            <div className="container mx-auto flex justify-between items-center">
-                <Link href="/" className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600">
+            <div className="container mx-auto flex justify-between items-center max-w-4xl">
+                <Link href="/" className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600 uppercase">
                     Casino
                 </Link>
-                <div>
-                    <AuthButton />
-                </div>
+                <AuthButton />
             </div>
         </nav>
     );
