b7021b8cc9459defc116690240c68eff3545350d
lunoxdev <lunox.code@gmail.com>
2025-09-18 22:57:07 -0600

    Add game covers and data structure: include multiple game cover images and create a data file to manage game information, enhancing the overall game catalog.

---
+++ src/data/data.ts
@@ -0,0 +1,160 @@
+import { type Games } from "../types/types";
+import cover1 from "../assets/games/cover-384x505-1.webp";
+import cover2 from "../assets/games/cover-384x505-2.webp";
+import cover3 from "../assets/games/cover-384x505-3.webp";
+import cover4 from "../assets/games/cover-384x505-4.webp";
+import cover5 from "../assets/games/cover-384x505-5.webp";
+import cover6 from "../assets/games/cover-384x505-6.webp";
+import cover7 from "../assets/games/cover-384x505-7.webp";
+import cover8 from "../assets/games/cover-384x505-8.webp";
+import cover9 from "../assets/games/cover-384x505-9.webp";
+import cover10 from "../assets/games/cover-384x505-10.webp";
+import cover11 from "../assets/games/cover-384x505-11.webp";
+import cover12 from "../assets/games/cover-384x505-12.webp";
+import cover13 from "../assets/games/cover-384x505-13.webp";
+import cover14 from "../assets/games/cover-384x505-14.webp";
+import cover15 from "../assets/games/cover-384x505-15.webp";
+import cover16 from "../assets/games/cover-384x505-16.webp";
+import cover17 from "../assets/games/cover-384x505-17.webp";
+import cover18 from "../assets/games/cover-384x505-18.webp";
+import cover19 from "../assets/games/cover-384x505-19.webp";
+import cover20 from "../assets/games/cover-384x505-20.webp";
+import jugglenau from "../assets/games/jugglenau.webp";
+import koiGate from "../assets/games/koi-gate.webp";
+import londonHunter from "../assets/games/london-hunter.webp";
+import newYearsBash from "../assets/games/new-years-bash.webp";
+import presto from "../assets/games/presto.webp";
+import waysOfFortune from "../assets/games/ways-of-fortune.webp";
+
+export const gamesData: Games[] = [
+  {
+    name: "Jugglenau",
+    url: "https://app-test.insvr.com/games/?brandid=a9d542ac-c9bb-e311-93f6-80c16e0883f4&brandgameid=f73fd882-9082-4e49-9bf9-0c3ed0f6ac84&mode=fun&locale=en&ifrm=1&lobbyurl=pm-closeGame",
+    cover: jugglenau,
+  },
+  {
+    name: "Koi Gate",
+    url: "https://app-test.insvr.com/games/?brandid=a9d542ac-c9bb-e311-93f6-80c16e0883f4&brandgameid=a103867f-7f21-467c-9208-5ee7149820cb&mode=fun&locale=en&ifrm=1&lobbyurl=pm-closeGame",
+    cover: koiGate,
+  },
+  {
+    name: "London Hunter",
+    url: "https://app-test.insvr.com/games/?brandid=a9d542ac-c9bb-e311-93f6-80c16e0883f4&brandgameid=20b52291-d316-4521-817b-f28863042bf1&mode=fun&locale=en&ifrm=1&lobbyurl=pm-closeGame",
+    cover: londonHunter,
+  },
+  {
+    name: "New Years Bash",
+    url: "https://app-test.insvr.com/games/?brandid=a9d542ac-c9bb-e311-93f6-80c16e0883f4&brandgameid=0ff2fec9-45b5-4150-9f0e-b0a22eb5d097&mode=fun&locale=en&ifrm=1&lobbyurl=pm-closeGame",
+    cover: newYearsBash,
+  },
+  {
+    name: "Presto",
+    url: "https://app-test.insvr.com/games/?brandid=a9d542ac-c9bb-e311-93f6-80c16e0883f4&brandgameid=12f9d75e-e7bf-49f7-ac63-d2c30adda93d&mode=fun&locale=en&ifrm=1&lobbyurl=pm-closeGame",
+    cover: presto,
+  },
+  {
+    name: "Ways of Fortune",
+    url: "https://app-test.insvr.com/games/?brandid=a9d542ac-c9bb-e311-93f6-80c16e0883f4&brandgameid=476fa6fb-63b0-4bfb-a42a-53899023c081&mode=fun&locale=en&ifrm=1&lobbyurl=pm-closeGame",
+    cover: waysOfFortune,
+  },
+  {
+    name: "Rip City",
+    url: "https://static-live.hacksawgaming.com/1233/1.31.2/index.html?language=es&channel=desktop&gameid=1233&mode=2&token=StakeDemoToken&lobbyurl=https%253a%252f%252fstake.com&currency=EUR&partner=stake&rcenable=0&rcelapsed=0&env=https://rgs-demo.hacksawgaming.com/api&realmoneyenv=https://rgs-cu.hacksawgaming.com/api&alwaysredirect=true",
+    cover: cover13,
+  },
+  {
+    name: "Tanked",
+    url: "https://nolimitcity.com/demo/Tanked?showNavbar=true",
+    cover: cover20,
+  },
+  {
+    name: "Blood Shadow",
+    url: "https://nolimitcity.com/demo/BloodAndShadow?showNavbar=true",
+    cover: cover14,
+  },
+  {
+    name: "The Cript",
+    url: "https://nolimitcity.com/demo/TheCrypt?showNavbar=true",
+    cover: cover18,
+  },
+  {
+    name: "Le Bandit",
+    url: "https://static-live.hacksawgaming.com/launcher/static-launcher-backend.html?gameid=1309&channel=mobile&mode=demo&currency=eur&language=es&lobbyurl=https%3a%2f%2fstake.com&token=StakeDemoToken&partner=stake&rcenable=false&rcelapsed=0&rcinterval=0",
+    cover: cover2,
+  },
+  {
+    name: "Pirate Bonanza 2",
+    url: "https://static-live.hacksawgaming.com/launcher/static-launcher-backend.html?gameid=1863&channel=desktop&mode=demo&currency=eur&language=es&lobbyurl=https%3a%2f%2fstake.com&token=StakeDemoToken&partner=stake&rcenable=false&rcelapsed=0&rcinterval=0",
+    cover: cover3,
+  },
+  {
+    name: "Starlight Princess",
+    url: "https://demogamesfree.eotofjxixi.net/gs2c/openGame.do?gameSymbol=vs20starlight&lang=es&cur=USD&lobbyUrl=https://stake.com/casino/home&cashierUrl=https://stake.com/casino/home?tab=deposit&currency=btc&modal=wallet&stylename=rare_stake&jurisdiction=99&treq=bRY5pk6ng9E1D0kS5VTXkmB76p4Gf68uZ7bSqyBmfvxgzhGGwSYViPeun5mcIagD&isGameUrlApiCalled=true&userId=demo",
+    cover: cover4,
+  },
+  {
+    name: "Zeus vs Hades",
+    url: "https://demogamesfree.eotofjxixi.net/gs2c/openGame.do?gameSymbol=vs15godsofwar&lang=es&cur=USD&lobbyUrl=https://stake.com/casino/home&cashierUrl=https://stake.com/casino/home?tab=deposit&currency=btc&modal=wallet&stylename=rare_stake&jurisdiction=99&treq=ogTisslrCSpFVqfRM7j1svAaXsNfEq3Cjr8PXrKyGYPhMJhJfZdGmhycKEETrqLR&isGameUrlApiCalled=true&userId=demo",
+    cover: cover5,
+  },
+  {
+    name: "Sugar Rush",
+    url: "https://demogamesfree.eotofjxixi.net/gs2c/openGame.do?gameSymbol=vs20sugarrush&lang=es&cur=USD&lobbyUrl=https://stake.com/casino/home&cashierUrl=https://stake.com/casino/home?tab=deposit&currency=btc&modal=wallet&stylename=rare_stake&jurisdiction=99&treq=qCLL7tGVtMer5UCdHXUCFrD1ITFbKgAtSrqi7F0DrxOLC4BNKO34QO6efczaOxAn&isGameUrlApiCalled=true&userId=demo",
+    cover: cover6,
+  },
+  {
+    name: "Fire Portals",
+    url: "https://demogamesfree.eotofjxixi.net/gs2c/openGame.do?gameSymbol=vs20portals&lang=es&cur=USD&lobbyUrl=https://stake.com/casino/home&cashierUrl=https://stake.com/casino/home?tab=deposit&currency=btc&modal=wallet&stylename=rare_stake&jurisdiction=99&treq=eGkil25SxDUuHo41efohtCIFyio0pzzGKp67ghRK7JbUFLxBQg6NC29o7xXecbN1&isGameUrlApiCalled=true&userId=demo",
+    cover: cover7,
+  },
+  {
+    name: "Forge of Olympus",
+    url: "https://demogamesfree.eotofjxixi.net/gs2c/openGame.do?gameSymbol=vs20forge&lang=es&cur=USD&lobbyUrl=https://stake.com/casino/home&cashierUrl=https://stake.com/casino/home?tab=deposit&currency=btc&modal=wallet&stylename=rare_stake&jurisdiction=99&treq=eByjYKp9GRDhvYybrk5fVl8GtsDvey4WDu8bCUS4OCcSH355sv9PbxDxxgnXzRCx&isGameUrlApiCalled=true&userId=demo",
+    cover: cover8,
+  },
+  {
+    name: "The Magic Cauldron",
+    url: "https://demogamesfree.eotofjxixi.net/gs2c/openGame.do?gameSymbol=vs20magicpot&lang=es&cur=USD&lobbyUrl=https://stake.com/casino/home&cashierUrl=https://stake.com/casino/home?tab=deposit&currency=btc&modal=wallet&stylename=rare_stake&jurisdiction=99&treq=mfmtXGUNml3UIiQM6j4vJ5I1jRol7bnl8lCRpdLVFkwgQOxErad1tbi129kyMJDp&isGameUrlApiCalled=true&userId=demo",
+    cover: cover9,
+  },
+  {
+    name: "Six Six Six",
+    url: "https://static-live.hacksawgaming.com/launcher/static-launcher-backend.html?gameid=1534&channel=mobile&mode=demo&currency=eur&language=es&lobbyurl=https%3a%2f%2fstake.com&token=StakeDemoToken&partner=stake&rcenable=false&rcelapsed=0&rcinterval=0",
+    cover: cover10,
+  },
+  {
+    name: "Hand of Anubis",
+    url: "https://static-live.hacksawgaming.com/launcher/static-launcher-backend.html?gameid=1164&channel=mobile&mode=demo&currency=eur&language=es&lobbyurl=https%3a%2f%2fstake.com&token=StakeDemoToken&partner=stake&rcenable=false&rcelapsed=0&rcinterval=0",
+    cover: cover11,
+  },
+  {
+    name: "Fist of Destruction",
+    url: "https://static-live.hacksawgaming.com/launcher/static-launcher-backend.html?gameid=1408&channel=mobile&mode=demo&currency=eur&language=es&lobbyurl=https%3a%2f%2fstake.com&token=StakeDemoToken&partner=stake&rcenable=false&rcelapsed=0&rcinterval=0",
+    cover: cover12,
+  },
+  {
+    name: "Punk Rocker",
+    url: "https://babylonstkn.evo-games.com/frontend/evo/r2/#demo=direct&provider=nlc&balance_id=demo-babylonstk000002-cGxheWluZ2d1ZXN0MDQwMQ%253D%253D-14ce7ec3-d4c8-4f72-b6de-6e8f5390f0b5&ua_launch_id=185db69a540dabeaedf5aa6c&game=punkrocker&table_id=punkrocker000000&app=promotions",
+    cover: cover15,
+  },
+  {
+    name: "X-Ways Hoarder",
+    url: "https://babylonstkn.evo-games.com/frontend/evo/r2/#demo=direct&provider=nlc&balance_id=demo-babylonstk000002-cGxheWluZ2d1ZXN0MDQzMg%253D%253D-d09c3146-c5a2-4d0a-adca-0720caacb949&ua_launch_id=185db6a0b2c2e3aff3c45ebe&game=xwayshoarder2&table_id=xwayshoarder2000&app=promotions",
+    cover: cover16,
+  },
+  {
+    name: "Rise of Ymir",
+    url: "https://static-live.hacksawgaming.com/launcher/static-launcher-backend.html?gameid=1616&channel=mobile&mode=demo&currency=eur&language=es&lobbyurl=https%3a%2f%2fstake.com&token=StakeDemoToken&partner=stake&rcenable=false&rcelapsed=0&rcinterval=0",
+    cover: cover17,
+  },
+  {
+    name: "True Kult",
+    url: "https://babylonstkn.evo-games.com/frontend/evo/r2/#demo=direct&provider=nlc&balance_id=demo-babylonstk000002-cGxheWluZ2d1ZXN0MDUxMw%253D%253D-4e37c0b2-d165-479b-ab31-302d1cdc372b&ua_launch_id=185db6b2bce621c44f007fba&game=truekult&table_id=truekult00000000&app=promotions",
+    cover: cover19,
+  },
+  {
+    name: "Wishbringer",
+    url: "https://static-live.hacksawgaming.com/launcher/static-launcher-backend.html?gameid=1711&channel=mobile&mode=demo&currency=eur&language=es&lobbyurl=https%3a%2f%2fstake.com&token=StakeDemoToken&partner=stake&rcenable=false&rcelapsed=0&rcinterval=0",
+    cover: cover1,
+  },
+];
---
+++ src/types/types.ts
@@ -0,0 +1,7 @@
+import { StaticImageData } from "next/image";
+
+export interface Games {
+  name: string;
+  url: string;
+  cover: StaticImageData;
+}
