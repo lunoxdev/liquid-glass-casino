68667ec7a64b263c76bac3521184e4e8259b6f09
lunoxdev <lunox.code@gmail.com>
2025-09-19 18:15:28 -0600

    Implement search functionality in GamesGrid component and add InputSpotlightBorder for enhanced user experience.

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -1,30 +1,48 @@
+"use client"
+
 import { gamesData } from "../data/data";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import InputSpotlightBorder from "./ui/InputSpotlightBorder";
 
 const GamesGrid = () => {
+    const [searchTerm, setSearchTerm] = useState<string>("");
+
+    const filteredGames = gamesData.filter((game) =>
+        game.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
-        <section className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3 group">
-            {gamesData.map((game) => (
-                <Link
-                    href={game.url}
-                    key={game.name}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="rounded-md shadow-lg transform hover:scale-105 transition duration-300 ease-in-out cursor-pointer group-hover:opacity-50 hover:!opacity-100"
-                >
-                    <Image
-                        src={game.cover}
-                        alt={game.name}
-                        width={384} // Based on cover-384x505.webp
-                        height={505} // Based on cover-384x505.webp
-                        layout="responsive"
-                        objectFit="cover"
-                        className="rounded-t-lg"
-                    />
-                </Link>
-            ))}
-        </section>
+        <div className="relative">
+            <div className="absolute -top-3 right-0 z-10 w-1/2 sm:w-1/3">
+                <InputSpotlightBorder
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
+            <section className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3 group pt-14">
+                {filteredGames.map((game) => (
+                    <Link
+                        href={game.url}
+                        key={game.name}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="rounded-md shadow-lg transform hover:scale-105 transition duration-300 ease-in-out cursor-pointer group-hover:opacity-50 hover:!opacity-100"
+                    >
+                        <Image
+                            src={game.cover}
+                            alt={game.name}
+                            width={384}
+                            height={505}
+                            layout="responsive"
+                            objectFit="cover"
+                            className="rounded-t-lg"
+                        />
+                    </Link>
+                ))}
+            </section>
+        </div>
     );
 };
 
